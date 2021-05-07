956272c37642bbab5a02b7a275f43e63096cef60
Nabil702 <77048196+Nabil702@users.noreply.github.com>
2021-05-07 16:58:16 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("I F O U N C T", 700,120)
+fill("lightblue")
+text("Hint: For performing a specific task !!", 700,150)
+    
+
+fill("white")
+textSize(15)
+text("S A D A T B A E", 400,120)
+fill("lightblue")
+text("Hint: For storing data!!", 400,150)
+
+}
